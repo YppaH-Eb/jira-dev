39f7be8714de6c4ce80a8b820c942ff402abf180
YppaH-Eb <weiyimingbb@163.com>
2021-02-14 21:20:25 +0800

    chore: 添加TS类型定义

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   //不能直接修改传入的对象
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (param, delay) => {
+export const useDebounce = (param: any, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(param);
   useEffect(() => {
     let timeout = setTimeout(() => setDebouncedValue(param), delay);
