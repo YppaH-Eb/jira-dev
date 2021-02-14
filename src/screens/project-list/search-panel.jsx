c2bbeb7fe21d5ee1201c138fd4dfd11e19c15695
YppaH-Eb <weiyimingbb@163.com>
2021-02-14 20:26:58 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const SearchPanel =({param,setParam,users})=>{
+  return <form action="">
+    <div>
+      <input type="text" value={param.name} onChange={evt=>setParam({...param,name:evt.target.value})}/>
+      <select value={param.personId} onChange={evt =>setParam({...param,personId: evt.target.value}) }>
+        <option value=''>负责人</option>
+        {
+          users.map(user=>{
+            return <option value={user.id}>{user.name}</option>
+          })
+        }
+      </select>
+    </div>
+  </form>
+}
