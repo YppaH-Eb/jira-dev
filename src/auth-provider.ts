70496a11953c2caaa8e53ddb2175452f50a64104
魏一鸣 <weiym@lenovoedu.com>
2021-03-11 13:25:58 +0800

    feat: 添加登录注册系统

--- src/auth-provider.ts
+++ src/auth-provider.ts
@@ -1,5 +1,6 @@
 // 在真实环境中如果使用了firebase这种第三方auth服务的话，本文件不需要开发者开发
 import { User } from "./screens/project-list/search-panel";
+import { AuthForm } from "./context/auth-context";
 const localStorageKey = "__auth_provider_token__";
 export const getToken = () => window.localStorage.getItem(localStorageKey);
 
@@ -8,8 +9,8 @@ export const handleUserResponse = ({ user }: { user: User }) => {
   window.localStorage.setItem(localStorageKey, user.token || "");
   return user;
 };
-export const login = (data: { username: string; password: string }) => {
-  fetch(`${apiUrl}/login`, {
+export const login = (data: { password: string; username: string }) => {
+  return fetch(`${apiUrl}/login`, {
     method: "POST",
     headers: {
       "Content-type": "application/json",
@@ -17,12 +18,14 @@ export const login = (data: { username: string; password: string }) => {
     body: JSON.stringify(data),
   }).then(async (response) => {
     if (response.ok) {
-      handleUserResponse(await response.json());
+      return handleUserResponse(await response.json());
+    } else {
+      return Promise.reject(await response.json());
     }
   });
 };
-export const register = (data: { username: string; password: string }) => {
-  fetch(`${apiUrl}/login`, {
+export const register = (data: AuthForm) => {
+  return fetch(`${apiUrl}/register`, {
     method: "POST",
     headers: {
       "Content-type": "application/json",
@@ -30,9 +33,12 @@ export const register = (data: { username: string; password: string }) => {
     body: JSON.stringify(data),
   }).then(async (response) => {
     if (response.ok) {
-      handleUserResponse(await response.json());
+      return handleUserResponse(await response.json());
+    } else {
+      return Promise.reject(await response.json());
     }
   });
 };
 
-export const logout = () => window.localStorage.removeItem(localStorageKey);
+export const logout = async () =>
+  window.localStorage.removeItem(localStorageKey);
